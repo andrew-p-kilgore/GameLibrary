803bafd692c06263811fc8f95f1500913a1bc3c4
andrew-p-kilgore <andrew.p.kilgore.56@gmail.com>
2021-08-10 09:16:03 -0500

    Update CustomerDao.java

--- GameLibrary-project/gameLibrary/src/main/java/com/jtakdn/gameLibrary/dao/CustomerDao.java
+++ GameLibrary-project/gameLibrary/src/main/java/com/jtakdn/gameLibrary/dao/CustomerDao.java
@@ -1,4 +1,17 @@
 package com.jtakdn.gameLibrary.dao;
 
+import com.jtakdn.gameLibrary.dto.Customer;
+
 public interface CustomerDao {
+
+    public Customer create();
+
+    public Customer get();
+
+    public Customer getAll();
+
+    public Customer update();
+
+    public Customer delete();
+
 }
