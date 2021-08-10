1fca2fad955be1ecc6a375bc301c7690d2fd2b79
andrew-p-kilgore <andrew.p.kilgore.56@gmail.com>
2021-08-10 09:25:25 -0500

    Added GameDaoImpl

---
+++ GameLibrary-project/gameLibrary/src/main/java/com/jtakdn/gameLibrary/dao/GameDaoImpl.java
@@ -0,0 +1,33 @@
+package com.jtakdn.gameLibrary.dao;
+
+import com.jtakdn.gameLibrary.dto.Game;
+
+import java.util.List;
+
+public class GameDaoImpl {
+
+    public Game create() {
+        Game game = new Game();
+        return game;
+    }
+
+    public Game get() {
+        Game game = new Game();
+        return game;
+    }
+
+    public List<Game> getAll() {
+        List<Game> gameList = null;
+        return gameList;
+    }
+
+    public Game update() {
+        Game game = new Game();
+        return game;
+    }
+
+    public Game delete() {
+        Game game = new Game();
+        return game;
+    }
+}
