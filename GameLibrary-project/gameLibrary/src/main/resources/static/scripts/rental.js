b9a1d0b17007dd4c960bfdf66014396930d01bd6
Johan Terblanche <johan.h.terblanche1@gmail.com>
2021-08-12 10:33:57 -0500

    min date on single rental as well

--- GameLibrary-project/gameLibrary/src/main/resources/static/scripts/rental.js
+++ GameLibrary-project/gameLibrary/src/main/resources/static/scripts/rental.js
@@ -1,4 +1,4 @@
-function minDate() {
+function minDates() {
     console.log("Logging Min Date Setting");
     var date = new Date(document.getElementById("newRentStart").value);
     date.setDate(date.getDate() + 1);
@@ -11,4 +11,18 @@ function minDate() {
           .toISOString()
           .split("T")[0];
     console.log("New Min Date: "+date.toISOString().split("T")[0]);
+}
+function minDate() {
+  console.log("Logging Min Date Setting");
+  var date = new Date(document.getElementById("editRentStart").value);
+  date.setDate(date.getDate() + 1);
+  document
+    .getElementById("editRentEnd")
+    .setAttribute("min", date.toISOString().split("T")[0]);
+  var endDate = new Date(document.getElementById("editRentEnd").value);
+  if (date >= endDate)
+    document.getElementById("editRentEnd").value = date
+      .toISOString()
+      .split("T")[0];
+  console.log("New Min Date: " + date.toISOString().split("T")[0]);
 }
